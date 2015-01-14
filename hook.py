aae9e056d7915e3c33297d505ebfc1bba370dcd7
Till Smejkal <till.smejkal+ycmtex@ossmail.de>
2015-01-14 12:22:48 +0100

    Added mail address to the copyright lines

--- hook.py
+++ hook.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 #
 # TexCompleter - Semantic completer for YouCompleteMe which handles Tex files.
-# Copyright (C) 2015 Till Smejkal
+# Copyright (C) 2015 Till Smejkal <till.smejkal@ossmail.de>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
